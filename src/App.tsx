917366b57a34e226c6f3c0b808f96a0b361c80ec
bryan <bryandrm97@gmail.com>
2020-01-24 17:53:22 -0600

    added style to set three cards on a row

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
+import './app.css'
 
 /* Theme variables */
 import './theme/variables.css';
