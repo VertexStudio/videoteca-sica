64243262a77fbdab20d4c9b89b266ce0897e8617
Kevin Rivera <kevinrev26@gmail.com>
2020-01-24 19:14:39 -0600

    Content example added

--- src/pages/home/Main.tsx
+++ src/pages/home/Main.tsx
@@ -10,9 +10,9 @@ class Home extends Component<{},{}>{
         return(
             <div id="container">
                 <Jumbotron />
-                <Card url="" title="" subtittle="" text="" thumbnail="" CardDescriptionStyle="cardDescription" />
-                <Card url="" title="" subtittle="" text="" thumbnail="" CardDescriptionStyle="cardDescription" />
-                <Card url="" title="" subtittle="" text="" thumbnail="" CardDescriptionStyle="cardDescription" />
+                <Card url="https://en.es-static.us/upl/2016/02/M42-Orion-Nebula-2-5-2016.jpg" title="Orion" subtittle="Orion nebula" text="The orion's nebula" thumbnail="" CardDescriptionStyle="cardDescription" />
+                <Card url="https://opengameart.org/sites/default/files/sun.png" title="The Sun" subtittle="Helios, Ra..." text="A example text" thumbnail="" CardDescriptionStyle="cardDescription" />
+                <Card url="https://magicforestworkshop.files.wordpress.com/2014/12/andromeda-galaxy.jpg" title="Andromeda" subtittle="Andromeda Galaxy" text="This galaxy is comming..." thumbnail="" CardDescriptionStyle="cardDescription" />
 
             </div>
             
