e05128588d388aae0735ea0e985cf327b52c95ed
Kevin Rivera <kevinrev26@gmail.com>
2020-01-27 10:41:16 -0600

    Navigation between main component and video player added

--- src/pages/home/Main.tsx
+++ src/pages/home/Main.tsx
@@ -2,17 +2,54 @@ import React, { Component} from 'react';
 import Jumbotron from "./header/Jumbotron";
 //import ArrayCard from "./ArrayCard/ArrayCardAndList";
 import Card from "./Card/card";
+import Player from "../video/player/Player";
+interface IMainState {
+    showVideo: boolean
+}
 
+class Home extends Component<{}, IMainState>{
 
-class Home extends Component<{},{}>{
+    constructor(props: any){
+        super(props);
+
+        this.state = {
+            showVideo: true
+        }
+    }
+
+    private backToHome = () => {
+        this.setState({ showVideo: false })
+    }
 
     render(){
+
+        if (this.state.showVideo){
+            return(
+            <Player 
+                url=""
+                onClose={this.backToHome}
+                />
+            )
+        }
+
         return(
             <div id="container">
                 <Jumbotron />
-                <Card url="https://en.es-static.us/upl/2016/02/M42-Orion-Nebula-2-5-2016.jpg" title="Orion" subtittle="Orion nebula" text="The orion's nebula" thumbnail="" CardDescriptionStyle="cardDescription" />
-                <Card url="https://opengameart.org/sites/default/files/sun.png" title="The Sun" subtittle="Helios, Ra..." text="A example text" thumbnail="" CardDescriptionStyle="cardDescription" />
-                <Card url="https://magicforestworkshop.files.wordpress.com/2014/12/andromeda-galaxy.jpg" title="Andromeda" subtittle="Andromeda Galaxy" text="This galaxy is comming..." thumbnail="" CardDescriptionStyle="cardDescription" />
+                <Card 
+                    url="https://en.es-static.us/upl/2016/02/M42-Orion-Nebula-2-5-2016.jpg" 
+                    title="Orion" subtittle="Orion nebula" text="The orion's nebula" 
+                    thumbnail="" 
+                    CardDescriptionStyle="cardDescription" />
+                <Card 
+                    url="https://opengameart.org/sites/default/files/sun.png" 
+                    title="The Sun" subtittle="Helios, Ra..." 
+                    text="A example text" thumbnail="" 
+                    CardDescriptionStyle="cardDescription" />
+                <Card 
+                    url="https://magicforestworkshop.files.wordpress.com/2014/12/andromeda-galaxy.jpg" 
+                    title="Andromeda" subtittle="Andromeda Galaxy" text="This galaxy is comming..." 
+                    thumbnail="" 
+                    CardDescriptionStyle="cardDescription" />
 
             </div>
             
--- src/pages/video/player/Player.tsx
+++ src/pages/video/player/Player.tsx
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import {Player} from 'video-react';
 import "./Player.css"
 import './video-react.css';
+//import currentVideo from "./assets/scene1.mp4";
 
 interface IProps {
     url: string;
+    onClose: () => void | undefined;
 };
 //interface IState {};
 
@@ -19,9 +21,10 @@ class VideoPlayer extends Component< IProps, {} > {
             <div className="video-container">
                 <h1>Reproductor</h1>
                 <Player
-                    src={this.props.url}
+                    src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
                     fluid={true}
                 />
+                <button onClick={this.props.onClose}>Close</button>
                     
                 
             </div>
