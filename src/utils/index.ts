fc6c00134d7a515a1148d73cb83cbd644db22f9e
bryan <bryandrm97@gmail.com>
2020-01-29 17:08:19 -0600

    Creation of gh-pages

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import IVideo from "./types";
-
-export const videoList: IVideo = {
-    "1":"./assets/scene1.mp4",
-    "2":"./assets/scene2.mp4",
-    "3":"./assets/scene3.mp4"
-}
---
+++ static/js/5.f1a3853f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvideoteca-sica"]=this["webpackJsonpvideoteca-sica"]||[]).push([[5],{281:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(39),o=(e(14),function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.g)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.m)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.f1a3853f.chunk.js.map
